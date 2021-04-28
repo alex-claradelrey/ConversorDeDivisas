3d516d0e1e33e434035d6defff6020965a483d35
juanjo-lv <juanjoselopezvega94@gmail.com>
2021-04-28 18:33:56 +0200

    Update conversor.py

--- conversor.py
+++ conversor.py
@@ -2,27 +2,37 @@
 dolar = 1
 euro = 1.21
 libra = 1.39
-btn = 54379.40 
+btc = 54379.40 
 
-conversiones = {"dolar":dolar,"euro":euro,"libra":libra,"btn":btn} #dolares
+conversiones = {"dolar":dolar,"euro":euro,"libra":libra,"btc":btc} #dolares
 
 def convertir_a_dollar(divisa,cant):
-    return resul = round(conversiones[divisa]*cant,3)
+     resul = round(conversiones[divisa]*cant,3)
+     return resul
     
 
 
 def conversor(divisa_ini,divisa_resul,cant):
     if divisa_ini and divisa_resul in conversiones:
         if(divisa_ini =="dolar"):
-           resul =  round(conversiones[divisa_ini]/cant,3)
+           resul= round(conversiones[divisa_ini]*cant,3)+" $"
         else:
-               
+            cantDollar = convertir_a_dollar(divisa_ini,cant)
+            resul =  round(cantDollar/conversiones[divisa_resul],3) 
+
+            if(divisa_resul=="euro"):
+                resul = str(resul)+"€"
+            elif(divisa_resul=="libra"):
+                resul = str(resul)+"£"
+            elif(divisa_resul=="btc"):
+                resul = str(resul)+"₿"
+            
+        return resul  
     else:
         print("error divisa no disponible")
-    return resul    
+      
 
-
-print(conversor("libra","euro",10))
+print(conversor("euro","btc",10000))
 
 
 
