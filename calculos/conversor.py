4e40d4d09cb19175f079b2fc3fe32c994adee5c1
juanjo-lv <juanjoselopezvega94@gmail.com>
2021-04-28 18:44:25 +0200

    Update conversor.py

--- calculos/conversor.py
+++ calculos/conversor.py
@@ -8,13 +8,26 @@ btc = 54379.40
 
 conversiones = {"dolar":dolar,"euro":euro,"libra":libra,"btc":btc} #dolares
 
+
 def convertir_a_dollar(divisa,cant):
+    '''
+        RECIBE -> El nombre de una divisa y una cantidad
+        DEVUELVE -> La cantidad convertida dollars
+    '''
      resul = round(conversiones[divisa]*cant,3)
      return resul
     
 
 
 def conversor(divisa_ini,divisa_resul,cant):
+    '''
+        RECIBE -> 
+            divisa_ini :    La divisa que queremos convertir
+            divisa_resul_ : La divisa a la que queremos convertir
+            cant:           Cantidad que deseamos convertir
+        DEVUELVE ->
+            la cantidad convertida a la divisa especificada con 3 cifras significativa y formateada a string con su simbolo correspondiente    
+    '''
     if divisa_ini and divisa_resul in conversiones:
         if(divisa_ini =="dolar"):
            resul= round(conversiones[divisa_ini]*cant,3)+"$"
@@ -33,8 +46,6 @@ def conversor(divisa_ini,divisa_resul,cant):
     else:
         print("error divisa no disponible")
       
-
-print(conversor("euro","btc",10000))
 
 
 
